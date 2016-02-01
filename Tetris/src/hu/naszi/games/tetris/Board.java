276e127cbdce2b388b499319a90cc82de40407ef
Naszi <naszi75@gmail.com>
2016-02-01 15:36:03 +0100

    Version 2 Create Board, Shape, Tetris classes

---
+++ Tetris/src/hu/naszi/games/tetris/Board.java
@@ -0,0 +1,5 @@
+package hu.naszi.games.tetris;
+
+public class Board {
+
+}
---
+++ Tetris/src/hu/naszi/games/tetris/Tetris.java
@@ -0,0 +1,10 @@
+package hu.naszi.games.tetris;
+
+public class Tetris {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
