4b5f23bc1d5054b3de34ef3b021303a5ef3d77ad
Naszi <naszi75@gmail.com>
2016-02-01 20:17:18 +0100

    Version 5 Shape.java is done.

--- Tetris/src/hu/naszi/games/tetris/Shape.java
+++ Tetris/src/hu/naszi/games/tetris/Shape.java
@@ -1,31 +1,28 @@
 package hu.naszi.games.tetris;
 
+import java.util.Random;
+
 public class Shape {
 	enum Tetrominoes {
 		NOSHAPE, ZSHAPE, SSHAPE, LINESHAPE, TSHAPE, SQUARESHAPE, LSHAPE, MIRROREDLSHAPE
 	};
-	
+
 	private Tetrominoes pieceShape;
 	private int coords[][];
-	private int [][][] coordsTable;
-	
+	private int[][][] coordsTable;
+
 	public Shape() {
 		coords = new int[4][2];
 		setShape(Tetrominoes.NOSHAPE);
 	}
 
 	private void setShape(Tetrominoes shape) {
-		coordsTable = new int[][][] {
-				{{0, 0}, {0, 0}, {0, 0}, {0, 0}},
-				{{0, -1}, {0, 0}, {-1, 0}, {-1, 1}},
-				{{0, -1}, {0, 0}, {1, 0}, {1, 1}},
-				{{0, -1}, {0, 0}, {0, 1}, {0, 2}},
-				{{-1, 0}, {0, 0}, {1, 0}, {0, 1}},
-				{{0, 0}, {1, 0}, {0, 1}, {1, 1}},
-				{{-1, -1}, {0, -1}, {0, 0}, {0, 1}},
-				{{1, -1}, {0, -1}, {0, 0}, {0, 1}},
-		};
-		
+		coordsTable = new int[][][] { { { 0, 0 }, { 0, 0 }, { 0, 0 }, { 0, 0 } },
+				{ { 0, -1 }, { 0, 0 }, { -1, 0 }, { -1, 1 } }, { { 0, -1 }, { 0, 0 }, { 1, 0 }, { 1, 1 } },
+				{ { 0, -1 }, { 0, 0 }, { 0, 1 }, { 0, 2 } }, { { -1, 0 }, { 0, 0 }, { 1, 0 }, { 0, 1 } },
+				{ { 0, 0 }, { 1, 0 }, { 0, 1 }, { 1, 1 } }, { { -1, -1 }, { 0, -1 }, { 0, 0 }, { 0, 1 } },
+				{ { 1, -1 }, { 0, -1 }, { 0, 0 }, { 0, 1 } }, };
+
 		for (int i = 0; i < 4; i++) {
 			for (int j = 0; j < 2; j++) {
 				coords[i][j] = coordsTable[shape.ordinal()][i][j];
@@ -34,4 +31,74 @@ public class Shape {
 		pieceShape = shape;
 	}
 
+	private void setX(int index, int x) {
+		coords[index][0] = x;
+	}
+
+	private void setY(int index, int y) {
+		coords[index][1] = y;
+	}
+
+	public int x(int index) {
+		return coords[index][0];
+	}
+
+	public int y(int index) {
+		return coords[index][1];
+	}
+
+	public Tetrominoes getShape() {
+		return pieceShape;
+	}
+
+	public void setRandomShape() {
+		Random random = new Random();
+		int x = Math.abs(random.nextInt()) % 7 + 1;
+		Tetrominoes[] values = Tetrominoes.values();
+		setShape(values[x]);
+	}
+
+	public int minX() {
+		int m = coords[0][0];
+		for (int i = 0; i < 4; i++) {
+			m = Math.min(m, coords[i][0]);
+		}
+		return m;
+	}
+
+	public int minY() {
+		int m = coords[0][1];
+		for (int i = 0; i < 4; i++) {
+			m = Math.min(m, coords[i][1]);
+		}
+		return m;
+	}
+
+	public Shape rotateLeft() {
+		if (pieceShape == Tetrominoes.SQUARESHAPE)
+			return this;
+
+		Shape result = new Shape();
+		result.pieceShape = pieceShape;
+
+		for (int i = 0; i < 4; ++i) {
+			result.setX(i, y(i));
+			result.setY(i, -x(i));
+		}
+		return result;
+	}
+
+	public Shape rotateRight() {
+		if (pieceShape == Tetrominoes.SQUARESHAPE)
+			return this;
+
+		Shape result = new Shape();
+		result.pieceShape = pieceShape;
+
+		for (int i = 0; i < 4; ++i) {
+			result.setX(i, -y(i));
+			result.setY(i, x(i));
+		}
+		return result;
+	}
 }
